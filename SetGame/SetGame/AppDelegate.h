72f0a66e0fda8bb1bd0e679bcd2415246cc2bd6a
YuryBandarchuk16 <yury.bandarchuk@gmail.com>
2017-03-06 18:41:57 +0200

    Project uploaded

---
+++ SetGame/SetGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SetGame
+//
+//  Created by Юрий Бондарчук on 3/6/17.
+//  Copyright © 2017 Yury Bandarchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SetGame/SetGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SetGame
+//
+//  Created by Юрий Бондарчук on 3/6/17.
+//  Copyright © 2017 Yury Bandarchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
